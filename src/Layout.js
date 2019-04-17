1316a494dd929cb664bbefd75a4e7e8e5068b553
Bryan Reagan <bryan.reagan@brms.wisc.edu>
2019-04-17 15:44:41 -0500

    SVG troubleshooting

--- src/Layout.js
+++ src/Layout.js
@@ -2,19 +2,22 @@ import React, { Component } from 'react'
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
 import theme from './theme'
 import { AppBar, Toolbar, Typography } from '@material-ui/core'
-import ReactSVG from 'react-svg';
+import SVG from 'react-inlinesvg';
 
 class Layout extends Component {
 
     render() {
         return (
+            
             <MuiThemeProvider theme={theme}>
                 <AppBar position="static">
                     <Toolbar variant="dense">
                         <Typography variant="h6" color="inherit">BRMS Vivarium Portal - WIMR</Typography>
                     </Toolbar>
                 </AppBar>
-                <ReactSVG src="./static/1485.svg" />
+                <SVG 
+                    src="1485.svg"
+                />
             </MuiThemeProvider>
         );
     }
