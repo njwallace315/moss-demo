8ac8bd800653d6bea56ff32ae0acd682bd52c1dc
Nathaniel Wallace <nate.wallace@brms.wisc.edu>
2019-04-18 09:10:26 -0500

    everythings working don't touch

--- src/Vivarium.js
+++ src/Vivarium.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import floorplan from './static/floormap.svg'
+import { Link } from 'react-router-dom'
 
 const Vivarium = props => {
     return (
         <div>
-            <img src={floorplan} style={{ display: 'block', height: '90%', width: '90%' }} />
+            <Link to="/B1451">
+                <img src={floorplan} style={{ display: 'block', height: '90%', width: '90%' }} />
+            </Link>
         </div>
     );
 };
